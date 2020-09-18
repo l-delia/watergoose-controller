286eb155592b98f78b47fee4b85a6728a95dad4a
l-delia <71524810+l-delia@users.noreply.github.com>
2020-09-18 16:06:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,11 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
     music.powerUp.play()
     light.setAll(0x00ff00)
 })
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.press(ArcadeButton.B)
+    music.powerUp.play()
+    light.setAll(0xff0000)
+})
 light.setBrightness(50)
 light.setAll(0xffffff)
 forever(function () {
