cdc24e4dc0b2f7b8efd0f37984d5c671d72f56dd
l-delia <71524810+l-delia@users.noreply.github.com>
2020-09-18 15:27:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
+    music.baDing.play()
     light.setBrightness(121)
     light.setAll(0x00ff00)
+    pause(100)
+    light.setAll(0xffffff)
 })
 forever(function () {
+    light.setAll(0xffffff)
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
 })
