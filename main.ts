55996e2dfff37b36e06e91afd6cf1992fd5f9ee7
l-delia <71524810+l-delia@users.noreply.github.com>
2020-09-25 11:07:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 input.buttonA.onEvent(ButtonEvent.Click, function () {
-    makerController.player1.press(ArcadeButton.A)
-    music.powerUp.play()
-    light.setAll(0x00ff00)
+    if (num == 0) {
+        makerController.player1.press(ArcadeButton.A)
+        music.powerUp.play()
+        light.setAll(0x00ff00)
+        num += 1
+    }
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
-    makerController.player1.press(ArcadeButton.B)
-    music.powerUp.play()
-    light.setAll(0xff0000)
+    if (num == 0) {
+        makerController.player1.press(ArcadeButton.B)
+        music.powerUp.play()
+        light.setAll(0xff0000)
+        num += 1
+    }
 })
+let num = 0
 light.setBrightness(50)
 light.setAll(0xffffff)
+num = 0
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
 })
