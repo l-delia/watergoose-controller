a4e9ef14e2400040a325eb7e0ea11665e971ebd9
m-skelton1 <66584362+m-skelton1@users.noreply.github.com>
2020-09-25 11:22:13 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,24 @@
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    makerController.player1.press(ArcadeButton.A)
-    music.powerUp.play()
-    light.setAll(0x00ff00)
-})
-input.buttonB.onEvent(ButtonEvent.Click, function () {
+input.buttonB.onEvent(ButtonEvent.LongClick, function () {
     makerController.player1.press(ArcadeButton.B)
     music.powerUp.play()
     light.setAll(0xff0000)
 })
-light.setBrightness(50)
-light.setAll(0xffffff)
-forever(function () {
+input.onGesture(Gesture.TiltLeft, function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+    light.showRing(
+    `blue blue blue blue blue black black blue black black`
+    )
 })
+input.buttonA.onEvent(ButtonEvent.LongClick, function () {
+    makerController.player1.press(ArcadeButton.A)
+    music.powerUp.play()
+    light.setAll(0x00ff00)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+    light.showRing(
+    `black black blue black black blue blue blue blue blue`
+    )
+})
+light.setBrightness(10)
+light.setAll(0xffffff)
