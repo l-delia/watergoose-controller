5df426c1c2d23fe49d7fef151a616b703e344df7
m-skelton1 <66584362+m-skelton1@users.noreply.github.com>
2020-09-25 11:30:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,30 @@
-input.buttonB.onEvent(ButtonEvent.LongClick, function () {
-    makerController.player1.press(ArcadeButton.B)
-    music.powerUp.play()
-    light.setAll(0xff0000)
-})
 input.onGesture(Gesture.TiltLeft, function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
     light.showRing(
     `blue blue blue blue blue black black blue black black`
     )
 })
-input.buttonA.onEvent(ButtonEvent.LongClick, function () {
-    makerController.player1.press(ArcadeButton.A)
-    music.powerUp.play()
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.setButton(ArcadeButton.A, true)
     light.setAll(0x00ff00)
+    music.powerUp.play()
+    pause(1000)
+    makerController.player1.setButton(ArcadeButton.A, false)
+    light.setAll(0xffffff)
 })
 input.onGesture(Gesture.TiltRight, function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
     light.showRing(
     `black black blue black black blue blue blue blue blue`
     )
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.setButton(ArcadeButton.B, true)
+    light.setAll(0xff0000)
+    music.powerUp.play()
+    pause(1000)
+    makerController.player1.setButton(ArcadeButton.A, false)
+    light.setAll(0xffffff)
 })
 light.setBrightness(10)
 light.setAll(0xffffff)
