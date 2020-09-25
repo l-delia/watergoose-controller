7b7b4563882238384764fa4d16bd2ece9bd569e6
m-skelton1 <66584362+m-skelton1@users.noreply.github.com>
2020-09-25 12:20:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+// This is for the game over screen, I'm not sure how we would activate it when that happens though.
+input.onSwitchMoved(SwitchDirection.Right, function () {
+    control.runInParallel(function () {
+        while (true) {
+            light.showAnimation(light.rainbowAnimation, 9500)
+        }
+    })
+    control.runInParallel(function () {
+        while (true) {
+            music.playMelody("E F G - A - G - ", 200)
+            music.playMelody("E F G - A - G - ", 200)
+            music.playMelody("E F G A G A G - ", 200)
+            music.playMelody("E F G - A - G - ", 200)
+        }
+    })
+})
 input.onGesture(Gesture.TiltLeft, function () {
     light.showRing(
     `blue blue blue blue blue black black blue black black`
@@ -6,7 +22,7 @@ input.onGesture(Gesture.TiltLeft, function () {
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.setButton(ArcadeButton.A, true)
     light.setAll(0x00ff00)
-    music.powerUp.play()
+    music.playMelody("G - A - - - - - ", 1000)
     pause(1000)
     makerController.player1.setButton(ArcadeButton.A, false)
     light.setAll(0xffffff)
@@ -19,12 +35,12 @@ input.onGesture(Gesture.TiltRight, function () {
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     makerController.player1.setButton(ArcadeButton.B, true)
     light.setAll(0xff0000)
-    music.powerUp.play()
+    music.playMelody("G - A - - - - - ", 1000)
     pause(1000)
     makerController.player1.setButton(ArcadeButton.A, false)
     light.setAll(0xffffff)
 })
-light.setBrightness(10)
+light.setBrightness(5)
 light.setAll(0xffffff)
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
